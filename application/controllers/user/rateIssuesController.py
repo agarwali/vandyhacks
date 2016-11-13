0a0cbfb85a64fcd6c3ad7a527f95e696b8d4befb
Aleksandra Cvetanovska <cvetanovskaa@berea.edu>
2016-11-13 06:00:30 +0000

    Changes to database

--- application/controllers/user/rateIssuesController.py
+++ application/controllers/user/rateIssuesController.py
@@ -41,8 +41,6 @@ def rateIssues ():
   elif request.method == "POST":
     data=request.form
     if data['button'] == "save":
-      user=User.get(User.UID==0)
-      print user.first_name
       issuesList = request.form.getlist('issueChoices')
       x=1
       issues=', '.join(issuesList)
@@ -51,48 +49,17 @@ def rateIssues ():
     elif data['button'] == "post":
       issue=Issues.create()
       for key in data:
-        if key=="Abortion":
-          issue.abortion=data[key] 
-        elif key=="Women":
+        if key=="Women":
           issue.womenAndMinorities=data[key] 
         elif key=="Same-Sex":
           issue.abortion=data[key] 
-        elif key=="Crime":
-          issue.crime=data[key] 
         elif key=="Guns":
           issue.guns=data[key] 
-        elif key=="Public":
-          issue.publicFaith=data[key] 
-        elif key=="Environment":
-          issue.environment=data[key]
-        elif key=="Campaign":
-          issue.campaignFinance=data[key]
         elif key=="Obamacare":
           issue.obamacare=data[key]
-        elif key=="Schools":
-          issue.schools=data[key]
-        elif key=="Energy":
-          issue.energy=data[key]
-        elif key=="Drugs":
-          issue.socialSecurity=data[key]
         elif key=="Immigration":
           issue.immigration=data[key]
-        elif key=="Taxes":
-          issue.taxes=data[key]
-        elif key=="Stimulus":
-          issue.stimulus=data[key]
-        elif key=="Social":
-          issue.socialSecurity=data[key]
-        elif key=="Trade":
-          issue.trade=data[key]
-        elif key=="Military":
-          issue.military=data[key]
-        elif key=="America":
-          issue.america=data[key]
-        elif key=="Foreign":
-          issue.foreignPolicy=data[key]
-          
-      issue.user=0;
+        issue.user=1
       issue.save()
       
       return redirect("user/upcomingElections") 
--- application/models/issuesModel.py
+++ application/models/issuesModel.py
@@ -3,26 +3,11 @@ from application.models.userModel import User
 class Issues (Model):
   IID                 = PrimaryKeyField()
   user                = ForeignKeyField(User,null=True)
-  abortion            = IntegerField(default=1)
   womenAndMinorities  = IntegerField(default=1)
   sameSexMarriage     = IntegerField(default=1)
-  publicFaith         = IntegerField(default=1)
-  environment         = IntegerField(default=1)
-  campaignFinance     = IntegerField(default=1)
-  crime               = IntegerField(default=1)
   guns                = IntegerField(default=1)
   obamacare           = IntegerField(default=1)
-  schools             = IntegerField(default=1)
-  energy              = IntegerField(default=1)
-  drugs               = IntegerField(default=1)
-  stimulus            = IntegerField(default=1)
-  taxes               = IntegerField(default=1)
   immigration         = IntegerField(default=1)
-  socialSecurity      = IntegerField(default=1)
-  trade               = IntegerField(default=1)
-  military            = IntegerField(default=1) 
-  america             = IntegerField(default=1) 
-  foreignPolicy       = IntegerField(default=1)
   
   # Integer field means it can only be pro or against
   # Float field means it can be anywhere between pro, against or neutral
